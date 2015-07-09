3e81d128f08c52fa87519ef784eab90fcda74380
Boris Naguet <boris.naguet@atos.net>
2015-07-09 15:33:39 +0200

    Test automatic deployment of P2 repo

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,11 @@ rm -rf p2Repo || exit 0;
 mkdir p2Repo;
 
 #copy content
-cp ./webtools-tapestry-repository/target/repository/* ./p2Repo/
+cp -rf ./webtools-tapestry-repository/target/repository/* ./p2Repo/
 
 # go to the out directory and create a *new* Git repo
 cd p2Repo
+ls
 git init
 
 # inside this git repo we'll pretend to be a new user
@@ -21,8 +22,12 @@ git config user.email "boris.naguet@atos.net "
 git add .
 git commit -m "Deploy to GitHub Pages"
 
+echo "commit done"
+
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+echo "push done"
