cd012c8323b7ba82e5f18449aa2294eaa92a1cdb
Boris Naguet <boris.naguet@atos.net>
2015-07-09 16:53:32 +0200

    renew encryption of password

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,12 @@ set -e # exit with nonzero exit code if anything fails
 # clear and re-create the out directory
 rm -rf p2Repo || exit 0;
 
-pwd
-ls
-ls webtools-tapestry-repository
-ls webtools-tapestry-repository/target
-ls webtools-tapestry-repository/target/repository
-
 #copy content
 cp -a webtools-tapestry-repository/target/repository/. p2Repo/
 
 # go to the p2Repo directory and create a *new* Git repo
 cd p2Repo
-ls
+
 git init
 
 # inside this git repo we'll pretend to be a new user
@@ -27,12 +21,8 @@ git config user.email "boris.naguet@atos.net "
 git add .
 git commit -m "Deploy to GitHub Pages"
 
-echo "commit done"
-
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
-
-echo "push done"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
